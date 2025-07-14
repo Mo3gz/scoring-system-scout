5d7720827a367d6d34f4c920bee3af84a1d8c8cb
Ayman Ashraf Mounir <sigmanup@gmail.com>
2025-07-14 04:19:56 +0300

    Updating files for deployment

--- server/index.js
+++ server/index.js
@@ -481,10 +481,10 @@ io.on('connection', (socket) => {
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
-  
+  app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
   });
 }
 
